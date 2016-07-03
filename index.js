2a302e238ff371cbc2ec002cb70f0a52821e2dfe
ross <rosshinkley@gmail.com>
2016-07-03 17:21:59 -0400

    adding solutions

--- index.js
+++ index.js
@@ -9,6 +9,26 @@ app.get('/hello/world', function(request, response){
   response.send('hello there!');
 });
 
+app.get('/a(pp)?le', function(request, response){
+  response.send('Apple or Ale?');
+});
+
+app.get('/who+a+', function(request, response){
+  response.send('I know, right!?');
+});
+
+app.get('/reverse/:word', function(request, response){
+  response.send(request.params.word.split('').reverse().join(''));
+});
+
+app.get('/:first/:last', function(request, response){
+  response.send(`hello, ${request.params.first} ${request.params.last}`);
+});
+
+app.get('*', function(request, response){
+  response.send('Uh oh!');
+});
+
 //have the application listen on a specific port
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
